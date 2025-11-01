7fe2485485f09d63ea7bc3d557d4623287a22c22
CC326297 <SahasransuB@nedbank.co.za>
2025-11-01 14:03:31 +0530

    estore

--- EstoreAngular/src/app/app.component.spec.ts
+++ EstoreAngular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'EstoreAngular' title`, () => {
+  it(`should have the 'estore' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EstoreAngular');
+    expect(app.title).toEqual('estore');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EstoreAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, estore');
   });
 });
